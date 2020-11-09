59307c48e6986ad498f948c5dee8b0cf1e07b368
Muhannad Abbasi <58631937+Muhannad-Abbasi@users.noreply.github.com>
2020-11-09 17:24:41 +0100

    Update inlämimgsuppigft.js

--- sinus-minishop/js/inlämimgsuppigft.js
+++ sinus-minishop/js/inlämimgsuppigft.js
@@ -6,7 +6,11 @@ homeReset.innerText = 'Reset';
 
 // 1 - B - Alla ändringar som du har gjort ska ändras tillbaka
 
+// Create an EventListener which call fanction resetAll() when we click on the "Reset" button on the top right of the page
+
 homeReset.addEventListener('click', resetAll)
+
+// Create an EventListener which call fanction firstChange() when we click on the "buy" button in the right card
 
 let buttonElement = document.querySelectorAll('button');
 let changeColorButton = buttonElement[2];
@@ -14,10 +18,14 @@ changeColorButton.addEventListener('click', function(event){
     firstChange()
 })
 
+// Create an EventListener which call fanction secondChange() when we click on the "buy" button in the middle card
+
 let changeBackgroundColorButton = buttonElement[1];
 changeBackgroundColorButton.addEventListener('click', function(event){
     secondChange()
 })
+
+// Create an EventListener which call fanction thirdChange() when we click on the "buy" button in the left card
 
 let changeButtonColor = buttonElement[0];
 changeButtonColor.addEventListener('click', function(event){
@@ -26,31 +34,31 @@ changeButtonColor.addEventListener('click', function(event){
 
 function resetAll(){
 
-    // 1
+    // 1 - Reset image for the right card to the original one
 
     let imgElement = document.querySelectorAll('img');
     imgElement[4].src = 'img/hoodie-ocean.png'
 
-    // 2
+    // 2 - Reset background color for the middle card to the original one
 
     let figureElement = document.querySelectorAll('figure');
     figureElement[1].style.backgroundColor = 'rgb(244, 182, 10)';
 
-    // 3
+    // 3 - Reset text description for the middle card to the original one
 
     let pElement = document.querySelectorAll('p');
     pElement[1].innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione neque sint quaerat consequatur laudantium!';
 
-    // 4
+    // 4 - Reset button color for the first card to the original one
 
     buttonElement[0].style.backgroundColor = '#222';
 
-    // 5
+    // 5 - Restore the element which we've deleted ( Social media's list )
 
     let articleElement = document.querySelectorAll('article');
     articleElement[5].style.display = 'block';
 
-    // 6
+    // 6 - Delete the list which we've created in the beginning
 
     let sectionElement = document.querySelector('section');
     sectionElement.removeChild(newUl);
@@ -94,14 +102,21 @@ function thirdChange(){
 
 
     let sectionElement = document.querySelector('section');
+
+// 1 - Create an unordered list and add it to element section
+
     let newUl = document.createElement('ul');
     sectionElement.appendChild(newUl);
+
+// 2 - Create five list item using for loop and add them into UI element which we've created previously
 
     for( i = 0; i < 5; i++){
         let newLi = document.createElement('li');
         newUl.appendChild(newLi)
     }
-    
+
+// 3 - Create an array to put the text inside the list item using for loop
+
     let liArr = ['Google', 'YouTube', 'Snapchat', 'Linkedln', 'Potato'];
 
     let liElements = document.querySelectorAll('li');
@@ -109,6 +124,8 @@ function thirdChange(){
     for(i = 0; i < liArr.length; i++){
         liElements[i].innerText = liArr[i];
     }
+
+// 4 - Decorate list item using style.border & style.listStyle into for loop
 
     let hue = 0;
     let hueStep = 360/liElements.length;
